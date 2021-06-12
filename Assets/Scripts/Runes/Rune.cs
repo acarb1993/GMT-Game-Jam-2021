ad9755d9ad378a3e96ccbc0908568781be190bbc
Adam Carballido <carballido.adam@yahoo.com>
2021-06-11 23:00:57 -0400

    Added scriptable objects for the runes and air rune art work

--- Assets/Scripts/Runes/Rune.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Rune : MonoBehaviour
-{
-    [SerializeField] protected float cost;
-}
---
+++ Assets/Scripts/Scriptable Objects Scripts/Rune.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Rune : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Sprite artwork;
+    public float cost;
+}
