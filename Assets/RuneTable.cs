ffc698345989244e429a37af164044aa9eceb242
Adam Carballido <carballido.adam@yahoo.com>
2021-06-12 21:34:34 -0400

    Added crafting UI sprite

--- Assets/RuneTable.cs
+++ Assets/RuneTable.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class RuneTable : MonoBehaviour
 {
     [SerializeField] private Transform tableUI;
+    private bool canCraft;
+
+    void Update()
+    {
+        
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
             tableUI.gameObject.SetActive(true);
+            canCraft = true;
         }
     }
 
@@ -17,6 +24,7 @@ public class RuneTable : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             tableUI.gameObject.SetActive(false);
+            canCraft = false;
         }
     }
 }
