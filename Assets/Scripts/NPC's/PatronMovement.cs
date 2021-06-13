d1b34fd832f198dc6a2dd2c3f92c7fe412356847
Amaligom78 <PrizeFighterInferno2113@hotmail.com>
2021-06-12 22:33:48 -0400

    Ignore

--- Assets/Scripts/NPC's/PatronMovement.cs
+++ Assets/Scripts/NPC's/PatronMovement.cs
@@ -1,22 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PatronMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 2;
-    private GameObject shopQueue;
-    private Transform queueSpot;
+    private GameObject shopEntrance;
     
     void Start()
     {
-        shopQueue = GameObject.Find("Shop Queue");
-        ShopQueue sq = shopQueue.GetComponent<ShopQueue>();
-
-        queueSpot = sq.CreatePosition().transform;
-        
+        shopEntrance = GameObject.Find("Shop Entrance");
     }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, queueSpot.position, Time.deltaTime * speed);
+        transform.position = Vector2.MoveTowards(transform.position, shopEntrance.transform.position, Time.deltaTime * speed);
     }
 }
